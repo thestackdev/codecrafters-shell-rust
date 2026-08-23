a5c779f826639a50e96b26f83aa90a392cfdd5b9
thestackdev <shanmukeshwar@icloud.com>
2026-08-23 21:05:47 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::FromStr;
+
+enum AvailableCommands {
+    Exit,
+}
+
+impl FromStr for AvailableCommands {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "exit" => Ok(Self::Exit),
+            _ => Err(format!("{}: command not found", s)),
+        }
+    }
+}
 
 fn main() {
     let mut command = String::new();
 
-    loop {
+    'main: loop {
         command.clear();
 
         print!("$ ");
         io::stdout().flush().unwrap();
-
         io::stdin().read_line(&mut command).unwrap();
 
-        println!("{}: command not found", command.trim());
+        let builtin: Result<AvailableCommands, String> = command.trim().parse();
+
+        match builtin {
+            Ok(AvailableCommands::Exit) => {
+                break 'main;
+            }
+            Err(e) => {
+                println!("{}", e);
+            }
+        }
     }
 }
