ba702b1211f2b105514d73323ff0aa66c4c0a804
thestackdev <shanmukeshwar@icloud.com>
2026-08-29 17:54:38 +0530

    codecrafters submit [skip ci]

--- src/utils.rs
+++ src/utils.rs
@@ -5,18 +5,23 @@ pub fn parse_args(s: &str) -> Vec<String> {
     let mut within_single_quote = false;
     let mut within_double_quote = false;
     let mut has_content = false;
+    let mut should_ignore_char = false;
 
     for ch in s.chars() {
         match ch {
-            '\"' if !within_single_quote => {
+            '\\' if !should_ignore_char => {
+                should_ignore_char = true;
+                has_content = true;
+            }
+            '\"' if !within_single_quote && !should_ignore_char => {
                 within_double_quote = !within_double_quote;
                 has_content = true;
             }
-            '\'' if !within_double_quote => {
+            '\'' if !within_double_quote && !should_ignore_char => {
                 within_single_quote = !within_single_quote;
                 has_content = true;
             }
-            ' ' if !within_single_quote && !within_double_quote => {
+            ' ' if !within_single_quote && !within_double_quote && !should_ignore_char => {
                 if has_content {
                     args.push(current.clone());
                     current.clear();
@@ -24,6 +29,7 @@ pub fn parse_args(s: &str) -> Vec<String> {
                 }
             }
             _ => {
+                should_ignore_char = false;
                 current.push(ch);
                 has_content = true;
             }
